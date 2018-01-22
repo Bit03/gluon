db422ecdad1f7b0128822959dd65afdc1e85a1a7
jiaxin <edison7500@gmail.com>
2018-01-22 14:22:33 +0800

    update

--- dapps/urls/__init__.py
+++ dapps/urls/__init__.py
@@ -3,6 +3,5 @@ from dapps.views import DAppsListView, DAppsDetailView
 
 urlpatterns = [
     url(r'^$', DAppsListView.as_view(), name='list'),
-    # url(r'^fakedetail\.htm$', DappsFakeDetailView.as_view(), name='detail_fake'),
     url(r'^(?P<slug>\d+)\.htm$', DAppsDetailView.as_view(), name='detail'),
 ]
