0710cf0a189f2ab4e53486be4fa0aaa19431a103
jiaxin <edison7500@gmail.com>
2018-01-29 19:59:00 +0800

    update

--- dapps/models.py
+++ dapps/models.py
@@ -21,21 +21,21 @@ class DApp(SoftDeletableModel):
 
     slug = fields.RandomCharField(length=12, unique=True,
                                   include_alpha=False, db_index=True, editable=False)
-    name = models.CharField(max_length=64, )
+    name = models.CharField(max_length=64,)
     platform = models.CharField(max_length=64, )
     symbol = models.CharField(max_length=64, null=True, blank=True)
-    description = models.TextField(blank=True, default='')
+    description = models.TextField(blank=True, default='',)
     description_cn = models.TextField(blank=True, default='')
-    country_of_origin = models.CharField(max_length=64, default='')
+    country_of_origin = models.CharField(max_length=64, default='', blank=True)
 
     faq = models.CharField(max_length=255, default='')
-    founder = models.CharField(max_length=255, default='')
-    vc = models.CharField(max_length=255, default='')
-    etherian = models.CharField(max_length=255, default='')
+    founder = models.CharField(max_length=255, default='', blank=True)
+    vc = models.CharField(max_length=255, default='', blank=True,)
+    etherian = models.CharField(max_length=255, default='', blank=True)
 
-    license = StatusField(choices_name='LICENSE_CHOICES')
-    status = StatusField(choices_name='STATUS')
-    ico_status = StatusField(choices_name='ICO_STATUS_CHOICES')
+    license = StatusField(choices_name='LICENSE_CHOICES', null=True, blank=True, default=None)
+    status = StatusField(choices_name='STATUS', null=True, blank=True)
+    ico_status = StatusField(choices_name='ICO_STATUS_CHOICES', null=True, blank=True, default=None)
 
     submitted = models.DateTimeField(null=True, blank=True, db_index=True)
     last_update = models.DateTimeField(null=True, blank=True, db_index=True)
@@ -63,10 +63,10 @@ class DApp(SoftDeletableModel):
 
 class ContractAddress(models.Model):
     dapp = models.OneToOneField(DApp, related_name='contract')
-    mainnet = models.CharField(max_length=128, null=True)
-    ropsten = models.CharField(max_length=128, null=True)
-    kovan = models.CharField(max_length=128, null=True)
-    rinkeby = models.CharField(max_length=128, null=True)
+    mainnet = models.CharField(max_length=128, null=True, blank=True)
+    ropsten = models.CharField(max_length=128, null=True, blank=True)
+    kovan = models.CharField(max_length=128, null=True, blank=True)
+    rinkeby = models.CharField(max_length=128, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Contract Address'
@@ -92,7 +92,7 @@ class Site(models.Model):
     dapp = models.OneToOneField(DApp, related_name='site')
     logo = models.URLField(max_length=255, null=True, blank=True)
     url = models.URLField(max_length=255, )
-    whitepaper = models.URLField(max_length=1024)
+    whitepaper = models.URLField(max_length=1024, blank=True, null=True)
 
     created_at = models.DateTimeField(default=timezone.now, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True, editable=False)
@@ -127,18 +127,18 @@ class GitHub(models.Model):
 
 class Social(models.Model):
     dapp = models.OneToOneField(DApp, related_name='social')
-    reddit = models.URLField(max_length=255, default='')
-    slack = models.URLField(max_length=255, default='')
-    gitter = models.URLField(max_length=255, default='')
-    blog = models.URLField(max_length=255, default='')
-    medium = models.URLField(max_length=255, default='')
-    wiki = models.URLField(max_length=255, default='')
-    twitter = models.URLField(max_length=255, default='')
-    facebook = models.URLField(max_length=255, default='')
-    telegram = models.URLField(max_length=255, default='')
-    youtube = models.URLField("Youtube", max_length=255, default='')
-    instagram = models.URLField("Instagram", max_length=255, default='')
-    linkedin = models.URLField("LinkedIn", max_length=255, default='')
-    bitcointalk = models.URLField("BitcoinTalk", max_length=255, default='')
-    google_plus = models.URLField("Google+", max_length=255, default='')
-    kakao = models.URLField("Kakao", max_length=255, default='')
+    reddit = models.URLField(max_length=255, default='', blank=True)
+    slack = models.URLField(max_length=255, default='', blank=True)
+    gitter = models.URLField(max_length=255, default='', blank=True)
+    blog = models.URLField(max_length=255, default='', blank=True)
+    medium = models.URLField(max_length=255, default='', blank=True)
+    wiki = models.URLField(max_length=255, default='', blank=True)
+    twitter = models.URLField(max_length=255, default='', blank=True)
+    facebook = models.URLField(max_length=255, default='', blank=True)
+    telegram = models.URLField(max_length=255, default='', blank=True)
+    youtube = models.URLField("Youtube", max_length=255, default='', blank=True)
+    instagram = models.URLField("Instagram", max_length=255, default='', blank=True)
+    linkedin = models.URLField("LinkedIn", max_length=255, default='', blank=True)
+    bitcointalk = models.URLField("BitcoinTalk", max_length=255, default='', blank=True)
+    google_plus = models.URLField("Google+", max_length=255, default='', blank=True)
+    kakao = models.URLField("Kakao", max_length=255, default='', blank=True)
