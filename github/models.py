fbaf4acc20f51f7e75f2470d9ae49236b11dc9f8
jiaxin <edison7500@gmail.com>
2018-01-30 20:26:47 +0800

    update

--- github/models.py
+++ github/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from dapps.models import DApp
 
+
 # Create your models here.
 
 
-class Github(models.Model):
+class Author(models.Model):
     dapp = models.ForeignKey(DApp, related_name='dapp')
     url = models.URLField(max_length=256, default='')
+    is_organization = models.BooleanField(default=False)
 
 
+class AuthorProfile(models.Model):
+    author = models.ForeignKey(Author, related_name='profile')
 
+
+class Repository(models.Model):
+    author = models.ForeignKey(Author, related_name='repository')
+    url = models.URLField(max_length=256, default='')
