f1f34daf86e617e968f2d67164c445aebb99ce5b
jiaxin <edison7500@gmail.com>
2018-02-07 12:20:12 +0800

    update

--- gluon/settings/settings.py
+++ gluon/settings/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
--- gluon/urls/api.py
+++ gluon/urls/api.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.schemas import get_schema_view
 
@@ -8,12 +8,15 @@ schema_view = get_schema_view(title='DAppRank API')
 
 urlpatterns = [
     url(r'^token-auth/?$', obtain_jwt_token),
+    url(r'^token-refresh/?$', refresh_jwt_token)
 ]
 
 urlpatterns += [
     url(r'^dapps/', include('dapps.urls.api', namespace='dapp')),
     url(r'^github/', include('github.urls.api', namespace='github')),
+]
 
+urlpatterns += [
     url(r'^schema/$', schema_view, name='schema'),
 ]
 
