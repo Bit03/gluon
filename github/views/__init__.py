c70aa2b28af4ea60e89c7f207e71126b21dc4daf
jiaxin <edison7500@gmail.com>
2018-03-19 17:06:15 +0800

    update

--- github/views/__init__.py
+++ github/views/__init__.py
@@ -2,6 +2,9 @@ import logging
 from django.views import generic
 from github.models import Organization, People, Repository
 
+import pygal
+from pygal.style import DarkSolarizedStyle
+
 logger = logging.getLogger('django')
 
 
@@ -33,3 +36,45 @@ class ReposDetailView(generic.DetailView):
     queryset = Repository.objects.all()
     slug_field = 'identified_code'
 
+    def get_context_data(self, **kwargs):
+        # _context = super(ProjectDetailView, self).get_context_data(**kwargs)
+
+        # _status = Status.objects.filter(project=self.object,
+        #                                 datetime__gte=datetime.now() - timedelta(31)
+        #                                 ).order_by('datetime')
+        # chart = self.get_chart(_status)
+        #
+        # _context.update({
+        #     'meta': {
+        #         'author': self.object.author,
+        #     },
+        #     'chart': chart.render_data_uri(show_dots=True),
+        # })
+        _context = super().get_context_data()
+        _chart = self.get_chart()
+
+        _context.update({
+            'chart': _chart.render_data_uri(show_dots=True),
+        })
+
+        return _context
+
+    def get_chart(self):
+        # df = stats.to_dataframe(index='datetime')
+        df = self.object.stats_df()
+
+        line_chat = pygal.Line(x_label_rotation=90,
+                               width=600, height=300,
+                               pretty_print=True,
+                               interpolate='cubic', style=DarkSolarizedStyle)
+        line_chat.human_readable = True
+        # line_chat.x_labels = map(lambda x: x.date.strftime("%Y-%m-%d"), )
+        line_chat.x_labels =  map(lambda x: x.strftime("%Y-%m-%d"), df.index.tolist())
+        star_se = df.star.diff().fillna(0)
+        fork_se = df.fork.diff().fillna(0)
+        watch_se = df.watch.diff().fillna(0)
+        line_chat.add('star', star_se.tolist())
+        line_chat.add('fork', fork_se.tolist())
+        line_chat.add('watch', watch_se.tolist())
+        line_chat.title = self.object.name
+        return line_chat
