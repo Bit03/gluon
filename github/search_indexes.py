91517a43dc5649517dc76b0f2ac6985ed1e32233
jiaxin <edison7500@gmail.com>
2018-03-21 20:52:13 +0800

    add sorted func

--- github/search_indexes.py
+++ github/search_indexes.py
@@ -11,12 +11,28 @@ class ReposIndex(indexes.Indexable, indexes.SearchIndex):
     created_at = indexes.DateTimeField(model_attr='created_at')
     updated_at = indexes.DateTimeField(model_attr='updated_at')
 
-    star = indexes.IntegerField(model_attr='star')
-    watch = indexes.IntegerField(model_attr='watch')
-    fork = indexes.IntegerField(model_attr='fork')
+    star = indexes.IntegerField(model_attr='star', default=0, stored=True)
+    watch = indexes.IntegerField(model_attr='watch', default=0, stored=True)
+    fork = indexes.IntegerField(model_attr='fork', default=0, stored=True)
+
+    latest_7_day_star = indexes.IntegerField(default=0, stored=True)
+    latest_7_day_fork = indexes.IntegerField(default=0, stored=True)
+    latest_7_day_watch = indexes.IntegerField(default=0, stored=True)
 
     def get_model(self):
         return Repository
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
+
+    def prepare_latest_7_day_star(self, obj):
+        star_sum = obj.stats_df(8).star.diff().fillna(0).sum()
+        return int(star_sum)
+
+    def prepare_latest_7_day_fork(self, obj):
+        fork_sum = obj.stats_df(8).fork.diff().fillna(0).sum()
+        return int(fork_sum)
+
+    def prepare_latest_7_day_watch(self, obj):
+        watch_sum = obj.stats_df(8).watch.diff().fillna(0).sum()
+        return int(watch_sum)
