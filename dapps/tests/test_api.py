21407bdac0b781894b46f47e64f5343748790df4
jiaxin <edison7500@gmail.com>
2018-05-23 20:46:06 +0800

    update travis.yml

--- dapps/tests/test_api.py
+++ dapps/tests/test_api.py
@@ -1,17 +1,30 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
+from faker import Faker
+
+from dapps.models import DApp
+from .faker_dapp import DappProvider
+
+fake = Faker()
+fake.add_provider(DappProvider)
 
 
 class DAppListAPIViewTestCase(APITestCase):
     def setUp(self):
-        pass
+        self.dapp = DApp()
+        self.dapp.name = fake.name()
+        self.dapp.platform = fake.word()
+        self.dapp.license = fake.license()
+        self.dapp.status = fake.status()
+        self.dapp.save()
 
     def tearDown(self):
-        pass
+        DApp.objects.all().delete()
 
     def test_can_get_dapp_list_from_api(self):
         _url = reverse('api:dapps:list')
         res = self.client.get(_url)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        print (res.json())
 
