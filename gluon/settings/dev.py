86395f9005adb56a03797ff5a4c0083d62797b2d
jiaxin <edison7500@gmail.com>
2018-05-23 14:01:38 +0800

    update

--- gluon/settings/dev.py
+++ gluon/settings/dev.py
@@ -1,3 +1,22 @@
-from .settings import *
+from .base import *  # noqa
+from .base import env
 
-SHELL_PLUS = "ptpython"
+SECRET_KEY = env('SECRET_KEY', default='g3tg^^e!o4fhaw1!ttjxn7j_9g@l@dsk2oe390c!g48oqkkk25')
+
+ALLOWED_HOSTS = [
+    "*",
+]
+
+# django-node
+# ------------------------------------------------------------------------------------
+# https://django-nose.readthedocs.io/en/latest/index.html
+INSTALLED_APPS += ['django_nose']
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=dapps,github',
+]
+
--- gluon/settings/stage.py
+++ gluon/settings/stage.py
@@ -1,1 +1,29 @@
-from .settings import *
+from .base import *  # noqa
+from .base import env
+
+SECRET_KEY = env('SECRET_KEY', default='g3tg^^e!o4fhaw1!ttjxn7j_9g@l@dsk2oe390c!g48oqkkk25')
+
+ALLOWED_HOSTS = [
+    "*",
+]
+
+
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ['127.0.0.1']
+
+
+# SHELL_PLUS = "ptpython"
