9b9609c0a808d8fa5bc440fb69fb5b3a47cf9f46
jiaxin <edison7500@gmail.com>
2018-06-12 13:41:44 +0800

    update

--- dapps/serializers.py
+++ dapps/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from dapps.models import DApp
+from dapps.models import DApp, Social
+
+
+class SocialSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Social
+        exclude = ["id", ]
 
 
 class DAppSerializers(serializers.ModelSerializer):
+    social = SocialSerializers()
+
     class Meta:
         model = DApp
-        exclude = ("updated_at", "created_at", "is_removed", )
+        exclude = ("updated_at", "created_at", "is_removed",)
--- gluon/urls/api.py
+++ gluon/urls/api.py
@@ -1,17 +1,22 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import (
+    verify_jwt_token,
+    refresh_jwt_token
+)
 from rest_framework_swagger.views import get_swagger_view
 
-
 urlpatterns = [
-    # url(r'^token-auth/?$', views.obtain_auth_token),
-    # url(r'^auth/', include('rest_auth.urls')),
     url(r'^docs/?$', get_swagger_view(title='DAppRank API'), name='docs'),
 ]
 
 urlpatterns += [
     url(r'^dapps/', include('dapps.urls.api', namespace='dapps')),
     url(r'^github/', include('github.urls.api', namespace='github')),
+
+    # auth api
+    url(r'^auth/verify/?$', verify_jwt_token, name='verify'),
+    url(r'^auth/refresh/?$', refresh_jwt_token, name='refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
