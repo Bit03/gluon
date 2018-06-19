0c9b3a3eca592a4b466da046e3276737af6bb6d4
jiaxin <edison7500@gmail.com>
2018-06-19 14:12:23 +0800

    add dapps detail slug

--- applications/dapps/urls/api.py
+++ applications/dapps/urls/api.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
-from applications.dapps.views.api import DAppListAPIView, DAppPlatformAPIView
+from applications.dapps.views.api import (
+    DAppListAPIView,
+    DAppPlatformAPIView,
+    DAppDetailAPIView,
+)
 
 
 urlpatterns = [
     url(r'^$', DAppListAPIView.as_view(), name='list'),
+    url(r'(?P<slug>\w+)/?$', DAppDetailAPIView.as_view(), name='detail'),
     url(r'^platform/?$', DAppPlatformAPIView.as_view(), name='platform'),
 ]
 
--- applications/dapps/views/api.py
+++ applications/dapps/views/api.py
@@ -9,6 +9,12 @@ class DAppListAPIView(generics.ListAPIView):
     filter_fields = ('status', 'ico_status', 'platform')
 
 
+class DAppDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = DAppSerializers
+    queryset = DApp.objects.all()
+    lookup_field = 'slug'
+
+
 class DAppPlatformAPIView(generics.ListAPIView):
     serializer_class = DAppPlatformSerializers
     queryset = DApp.objects.exclude(platform="").values('platform').distinct()
