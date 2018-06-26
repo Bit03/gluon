534a04afba79dc9d6e4ed123796d50766c08bd69
jiaxin <edison7500@gmail.com>
2018-06-26 10:35:12 +0800

    update

--- applications/github/management/commands/fetch_repos_commit.py
+++ applications/github/management/commands/fetch_repos_commit.py
@@ -1,9 +1,32 @@
+import logging
+import requests
+import time
 from django.core.management import BaseCommand
 from applications.github.models import Repository
+
+
+logger = logging.getLogger('django')
+
+spider_url = "https://spider.dapprank.com/schedule.json"
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for row in Repository.objects.all():
+        for row in Repository.objects.filter(html_url__isnull=False):
             print(row.full_name, row.html_url, row.id)
+
+            data = {
+                "project": "muon",
+                "spider": "commit",
+                "repos_id": row.id,
+                "url": row.html_url,
+            }
+
+            res = requests.post(spider_url, data=data, auth=('spider', 'spider1@#'))
+            if res.status_code == 200:
+                logger.info(res.json())
+            else:
+                logger.error(res.text)
+                # print(res.text)
+            time.sleep(3)
