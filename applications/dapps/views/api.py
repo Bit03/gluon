b683c87821332392cb3d67f31fdafb5e8515ae21
jiaxin <edison7500@gmail.com>
2018-07-02 19:57:11 +0800

    update

--- applications/dapps/views/api.py
+++ applications/dapps/views/api.py
@@ -15,17 +15,38 @@ class DAppDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = DApp.objects.all()
     lookup_field = 'slug'
 
-    def get_serializer(self, *args, **kwargs):
+    def get_serializer_class(self):
         """
-        Return the serializer instance that should be used for validating and
-        deserializing input, and for serializing output.
+        Return the class to use for the serializer.
+        Defaults to using `self.serializer_class`.
+
+        You may want to override this if you need to provide different
+        serializations depending on the incoming request.
+
+        (Eg. admins get full serialization, others get basic serialization)
         """
-        if self.request.user.is_supperuser:
-            serializer_class = AdminDAppSerializers()
+        if self.request.user.is_superuser:
+            return AdminDAppSerializers
         else:
-            serializer_class = self.get_serializer_class()
-        kwargs['context'] = self.get_serializer_context()
-        return serializer_class(*args, **kwargs)
+            assert self.serializer_class is not None, (
+                "'%s' should either include a `serializer_class` attribute, "
+                "or override the `get_serializer_class()` method."
+                % self.__class__.__name__
+            )
+
+            return self.serializer_class
+
+    # def get_serializer(self, *args, **kwargs):
+    #     """
+    #     Return the serializer instance that should be used for validating and
+    #     deserializing input, and for serializing output.
+    #     """
+    #     if self.request.user.is_superuser:
+    #         serializer_class = AdminDAppSerializers
+    #     else:
+    #         serializer_class = self.get_serializer_class()
+    #     kwargs['context'] = self.get_serializer_context()
+    #     return serializer_class(*args, **kwargs)
 
 
 class DAppPlatformAPIView(generics.ListAPIView):
