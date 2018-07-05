e96874e5693ab6e56872e3b92df19d16bb0a5166
jiaxin <edison7500@gmail.com>
2018-07-05 12:29:49 +0800

    update

--- applications/github/management/commands/update_github_project.py
+++ applications/github/management/commands/update_github_project.py
@@ -2,7 +2,7 @@ import logging
 from time import sleep
 
 from django.core.management import BaseCommand
-from applications.github.models import People
+from applications.github.models import People, Repository
 from github import Github, UnknownObjectException
 
 logger = logging.getLogger('django')
@@ -25,6 +25,16 @@ class Command(BaseCommand):
                 continue
 
             for repo in user.get_repos():
-                print(repo.name)
+                print(repo.name, repo.full_name, repo.html_url)
+
+                try:
+                    Repository.objects.get(author=row.login, name=repo.name)
+                except Repository.DoesNotExist:
+                    Repository.objects.create(
+                        author=row.login,
+                        name=repo.name,
+                        url=repo.url,
+                        html_url=repo.html_url,
+                    )
 
             sleep(3)
