de2f730ca3ad5282700c86639dc17ae9dfc4817d
jiaxin <edison7500@gmail.com>
2018-07-18 11:53:32 +0800

    update

--- applications/github/urls/api/people.py
+++ applications/github/urls/api/people.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from applications.github.views.api import (
     PeopleListAPIView,
-    PeopleDetailAPIView
+    PeopleDetailAPIView,
+    PeopleRankSerializer,
 )
 
 
 urlpatterns = [
     url(r'^$', PeopleListAPIView.as_view(), name='list'),
-    url(r'^rank/?$', PeopleDetailAPIView.as_view(), name='rank'),
+    url(r'^rank/?$', PeopleRankSerializer.as_view(), name='rank'),
     url(r'^(?P<login>[\-|\w]+)/?$', PeopleDetailAPIView.as_view(), name='detail'),
 ]
