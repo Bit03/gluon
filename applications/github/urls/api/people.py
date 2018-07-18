34a48a7c792514604c8d94d4dd4bde8445813032
jiaxin <edison7500@gmail.com>
2018-07-18 11:59:20 +0800

    fixed bug

--- applications/github/urls/api/people.py
+++ applications/github/urls/api/people.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from applications.github.views.api import (
     PeopleListAPIView,
     PeopleDetailAPIView,
-    PeopleRankSerializer,
+    PeopleRankAPIView,
 )
 
 
 urlpatterns = [
     url(r'^$', PeopleListAPIView.as_view(), name='list'),
-    url(r'^rank/?$', PeopleRankSerializer.as_view(), name='rank'),
+    url(r'^rank/?$', PeopleRankAPIView.as_view(), name='rank'),
     url(r'^(?P<login>[\-|\w]+)/?$', PeopleDetailAPIView.as_view(), name='detail'),
 ]
